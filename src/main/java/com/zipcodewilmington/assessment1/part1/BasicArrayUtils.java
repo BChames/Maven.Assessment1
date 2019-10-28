dbfe83ffc8ed4cacdad93daba025dd4054e0fce8
Brandon Chambers <brandonmchambers36@gmail.com>
2019-10-28 09:34:30 -0400

    complete BasicArrayUtils

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicArrayUtils.java
@@ -17,7 +17,7 @@ public class BasicArrayUtils {
      * @return the second element in the array
      */
     public static String getSecondElement(String[] stringArray) {
-        return null;
+        return stringArray[1];
     }
 
     /**
@@ -25,7 +25,10 @@ public class BasicArrayUtils {
      * @return the last element in the array
      */
     public static String getLastElement(String[] stringArray) {
-        return null;
+
+
+
+        return stringArray[ stringArray.length - 1];
     }
 
     /**
@@ -33,6 +36,6 @@ public class BasicArrayUtils {
      * @return the second to last element in the array
      */
     public static String getSecondToLastElement(String[] stringArray) {
-        return null;
+        return stringArray[ stringArray.length - 2];
     }
 }
